8c22c0113495f476622a73ea9a116a61f5510846
Festus Agboma <festusyuma@gmail.com>
2022-06-22 20:39:37 +0100

    fix: valid values in joi schema fix

--- src/app/schema/token.ts
+++ src/app/schema/token.ts
@@ -5,5 +5,5 @@ export const GenerateTokenSchema = Joi.object<GenerateTokenRequest>({
   key: Joi.string().required(),
   data: Joi.object().default({}),
   username: Joi.string().email().required(),
-  mode: Joi.string().valid(TokenMode).default(TokenMode.VIEWER)
+  mode: Joi.string().valid('PRESENTER', 'VIEWER', 'GUEST').default(TokenMode.VIEWER)
 })
