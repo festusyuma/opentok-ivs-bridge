71fd8d530a99072fd1da9b1e0070dc71ca15fb68
Festus Agboma <festusyuma@gmail.com>
2022-06-28 13:16:18 +0100

    consolelog apikey

--- src/app/modules/opentok.ts
+++ src/app/modules/opentok.ts
@@ -3,6 +3,4 @@ import OpenTok from 'opentok';
 const apiKey = process.env.OPENTK_API_KEY || ''
 const apiSecret = process.env.OPENTK_SECRET || ''
 
-console.log(apiKey)
-
 export const opentok = new OpenTok(apiKey, apiSecret)
